6db884d26de88c04b8bc0e9b8c19fac0153dfcb1
Ahmed Farag <farag0336@gmail.com>
2025-03-31 11:25:01 +0200

    Product list UI with Angular & connected to Spring backend

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list-table.component.html',
+  // templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit{
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService, private cd: ChangeDetectorRef) { }
+
+ngOnInit(): void {
+  this.listProducts();
+}
+listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+
+        console.log("Products received: ", data);
+        this.products = data;
+        this.cd.detectChanges();
+      },
+      error => {
+        console.error("Error fetching products: ", error);
+      }
+    );
+  }
+}
+
+
