1c1f9f8081fc957776af4b333a3bbae60bc1e8fe
Ahmed Farag <farag0336@gmail.com>
2025-04-05 04:40:56 +0200

    add Shopping Cart Items CRUD

--- src/app/components/cart-status/cart-status.component.ts
+++ src/app/components/cart-status/cart-status.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-cart-status',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './cart-status.component.html',
   styleUrl: './cart-status.component.css'
 })
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,19 +3,17 @@ import { CartItem } from '../common/cart-item';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
 
-  constructor() { }
+  constructor() {}
 
   addToCart(theCartItem: CartItem) {
-
     // check if we alredy have the item in our cart
     let alreadyExistsInCart: boolean = false;
     let existingCartItem: CartItem | undefined;
@@ -23,8 +21,7 @@ export class CartService {
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
 
-
-/*
+      /*
       for (let tempCartItem of this.cartItems) {
         if (tempCartItem.id === theCartItem.id) {
           existingCartItem = tempCartItem;
@@ -33,16 +30,17 @@ export class CartService {
       }
 */
 
-      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id);
+      existingCartItem = this.cartItems.find(
+        (tempCartItem) => tempCartItem.id === theCartItem.id
+      );
       // check if we found it
-      alreadyExistsInCart = (existingCartItem != undefined);
+      alreadyExistsInCart = existingCartItem != undefined;
     }
 
     if (alreadyExistsInCart) {
       // increment the quantity
       existingCartItem!.quantity++;
-    }
-    else {
+    } else {
       // just add the item to the array
       this.cartItems.push(theCartItem);
     }
@@ -52,11 +50,10 @@ export class CartService {
   }
 
   computeCartTotals() {
-
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
-    for(let currentCartItem of this.cartItems) {
+    for (let currentCartItem of this.cartItems) {
       totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
       totalQuantityValue += currentCartItem.quantity;
     }
@@ -67,17 +64,45 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
-
   }
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
-
     console.log('Contents of the cart');
     for (let tempCartItem of this.cartItems) {
       const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
-      console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
+      console.log(
+        `name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`
+      );
 
-      console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`)
+      console.log(
+        `totalPrice: ${totalPriceValue.toFixed(
+          2
+        )}, totalQuantity: ${totalQuantityValue}`
+      );
       console.log('---');
+    }
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(
+      (tempCartItem) => tempCartItem.id === theCartItem.id
+    );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
     }
   }
 }
