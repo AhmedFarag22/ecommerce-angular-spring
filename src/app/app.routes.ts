02280853be3050ce37947f11bf5ed0e56753dd91
Ahmed Farag <farag0336@gmail.com>
2025-04-13 20:26:56 +0200

    add VIP member access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,35 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
-import { OktaCallbackComponent } from '@okta/okta-angular'
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular'
 
 
 
 
 
 import { LoginStatusComponent } from './components/login-status/login-status.component';
+import OktaAuth from '@okta/okta-auth-js';
+import { Injector } from '@angular/core';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 
+function sendToLoginPage(oktaAuth: OktaAuth) { // function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector)
+  // Use injector to access any service available within your application
+  //const router = injector.get(Router);
 
+  // Redirect the user to your custom login page
+  //router.navigate(['/login']);
+  oktaAuth.signInWithRedirect();
+}
 
 export const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
+                    data: {onAuthRequired: sendToLoginPage} },
+
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginStatusComponent},
 
---
+++ src/app/components/members-page/members-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-members-page',
+
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './members-page.component.html',
+  styleUrl: './members-page.component.css'
+})
+export class MembersPageComponent {
+
+}
